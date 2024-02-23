e79312627fdc947301a5bed4b7b3dbe5cbe93bf2
RobertoScarci <robertoscarci.02@gmail.com>
2024-02-23 17:49:36 +0100

    implemented first part of homepage

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,34 +1,50 @@
 <template lang="">
-    <header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#"><img src="https://banner2.cleanpng.com/20180708/bs/kisspng-bacardi-cocktail-martini-cocktail-glass-logo-cocktail-logo-5b42630719a652.9189521615310773831051.jpg" alt="Logo" width="50" height="40" class="d-inline-block align-text-top"></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Cocktails</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Chi Siamo</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contattaci</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-primary" type="submit">Search</button>
-      </form>
+  <header>
+    <div class="p-2 mb-2 bg-black text-white text-center">25% DI SCONTO SUL QUINTO COCKTAIL!</div>
+      <nav class="navbar navbar-expand-lg mb-1">
+          <div class="container-fluid">
+          <a class="navbar-brand" href="#"><img src="https://www.shutterstock.com/image-vector/bar-lettering-illustration-label-badge-600nw-1034296870.jpg" alt="Logo" width="80" height="50" class="d-inline-block align-text-top"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Cocktails</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Chi Siamo</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contattaci</a>
+            </li>
+          </ul>
+        <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-primary" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  <div class="">
+    <div class="row ">
+      <div class="col-6 p-0">
+        <img src="https://www.coqtailmilano.com/wp-content/uploads/2021/04/cocktail-caraibici-ricette-ingredienti-preparazione-Coqtail-Milano.jpg" alt="" class="">
+      </div>
+      <div class="col-6 bg-body-tertiary border border-1 text-center vertical-align-center">
+        <h2>
+          PROVA IL NOSTRO BOX DI COCKTAIL
+        </h2>
+        <p>
+          Cinque iconici cocktail in busta pronti da bere, racchiusi in un elegante cofanetto trasportabile ovunque. Perfetto come idea regalo per chi ama sorseggiare il proprio drink preferito in compagnia.
+        </p>
+      </div>
     </div>
   </div>
-</nav>
-    </header>
+  </header>
 </template>
 
 <script>
@@ -36,8 +52,14 @@
 
 <style lang="scss" scoped>
 
+div,
 nav{
     font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
 }
+
+div.row img{
+    width: 100%;
+}
+
 
 </style>
