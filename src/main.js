0b3c2a22c5fde083d374f55c32c5d3f82fd04ca0
RobertoScarci <robertoscarci.02@gmail.com>
2024-02-25 14:01:16 +0100

    implementazione del router nel js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import './style/general.scss'
 
 import { createApp } from 'vue'
+import {router} from './router';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app');
